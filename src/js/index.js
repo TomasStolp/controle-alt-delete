451a1dd1ca61f48d32bc9f82dca9971e8043f7d0
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-01-19 21:39:55 +0100

    added style and positioning

--- src/js/index.js
+++ src/js/index.js
@@ -21,9 +21,9 @@ const margin = {
     left: 5
 };
 
-var width = window.innerWidth - margin.left - margin.right;
+var width = 1230 - margin.left - margin.right;
 
-var height = window.innerHeight - margin.top - margin.bottom;
+var height = 500 - margin.top - margin.bottom;
 
 // const svg = d3.select("svg")
 //     .attr("width", width + margin.left + margin.right)
@@ -45,11 +45,20 @@ data.then(data => data.json())
 
 function initDrawing(data) {
 
-    const nestedMigrationBg = hasMigrationBg(data);
-    const nestedRightfulness = formatData(nestedMigrationBg);
+    const migrationBackground = hasMigrationBg(data);
+    const nestedMigrationBg = d3.nest()
+                                    .key(d => d.migratieachtergrond)
+                                    .entries(migrationBackground);
 
+    const nestedRightfulness = formatData(data);
 
-    nestedRightfulness.forEach(e =>{
+    const migrationKeys = nestedMigrationBg.map(d => d.key)
+        console.log(nestedMigrationBg)
+    const colorscale = d3.scaleOrdinal()
+        .domain(migrationKeys)
+        .range(d3.schemeCategory10);
+
+    nestedRightfulness.forEach(e => {
         e.length = e.values.length;
     })
 
@@ -59,36 +68,36 @@ function initDrawing(data) {
     const priority_order = ['Zeer terecht', 'Terecht', 'Niet terecht, niet onterecht', 'Onterecht', 'Zeer onterecht']
     const doubleNest = d3.nest()
         .key(d => d.stel_terecht)
-        .sortKeys(function(a,b) { 
+        .sortKeys(function (a, b) {
             // console.log(priority_order[(priority_order.indexOf(a) - priority_order.indexOf(b))])
-            return priority_order.indexOf(a) - priority_order.indexOf(b); 
+            return priority_order.indexOf(a) - priority_order.indexOf(b);
         })
         .key(d => d.migratieachtergrond)
-        .entries(nestedMigrationBg);
+        .entries(migrationBackground);
 
- 
-        
-        doubleNest.forEach((e) => {
-            e.values.forEach((e, i) =>{
-             
-                e.values.forEach((d, i)=>{
-                    console.log(i)
-                    d.plek = i;
-                    d.parentLength = e.values.length;
-                    // e.values.forEach((e, i)=>{
-                       
-                    // })
-                   
-                })
+
+
+    doubleNest.forEach((e) => {
+        e.values.forEach((e, i) => {
+
+            e.values.forEach((d, i) => {
+                // console.log(i)
+                d.plek = i;
+                d.parentLength = e.values.length;
+                // e.values.forEach((e, i)=>{
+
+                // })
+
             })
         })
+    })
 
 
-        console.log(doubleNest      )
+    console.log(doubleNest)
 
-        // console.log(doubleNest)
+    // console.log(doubleNest)
 
-   
+
 
     // const dropdown = d3.select('select');
     // dropdown
@@ -122,13 +131,14 @@ function initDrawing(data) {
     }).keys());
 
 
-    
+
 
 
     //  var xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisRight(y);
+    const yAxis = d3.axisLeft(y);
 
-    // yAxis.tickSize(200)
+    yAxis.tickSize(-1300);
+  
 
 
     // console.log(nestedRightfulness.filter(e => e.key =()= 'Terecht')[0].values.length)
@@ -158,10 +168,9 @@ function initDrawing(data) {
 
     const svg = d3.select("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("viewBox", `0 0 800 900`)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("height", height + margin.top + margin.bottom);
+        // .attr("viewBox", `0 0 800 900`)
+        svg.append("g");
 
     // From https://bl.ocks.org/chloerulesok/e45c8bb1241c4f6051ef30623e6fe552
     // Author: chloerulesok’s
@@ -174,23 +183,23 @@ function initDrawing(data) {
     //     .attr("y", 0);
 
 
-    var colour = d3.scaleOrdinal()
-    .domain(d3.map(nestedMigrationBg, function(d){      
-        return d.key;
-    }))
-    .range(d3.schemeCategory10);
 
+    // const test = d3.map(nestedMigrationBg, function (d) {
 
-    const test = d3.map(nestedMigrationBg, function(d){
-        
-        return d.key;
-    })
+    //     return d.key;
+    // })
 
     // console.log(test)
 
 
     const scatter = svg.append("g")
         .attr("id", "scatterplot");
+
+        scatter.append("rect")
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('width', '100%')
+          .attr('height', '100%');
 
     // From https://bl.ocks.org/chloerulesok/e45c8bb1241c4f6051ef30623e6fe552
     // Author: chloerulesok’s
@@ -212,80 +221,166 @@ function initDrawing(data) {
             // console.log( y(10000))
             return d.idealy;
         }))
-        // .force("collide", d3.forceCollide(1)
-        //     .strength(0)
-        //     .iterations(2))
-        // .stop();
+    // .force("collide", d3.forceCollide(1)
+    //     .strength(0)
+    //     .iterations(2))
+    // .stop();
 
     // From https://bl.ocks.org/chloerulesok/e45c8bb1241c4f6051ef30623e6fe552
     // Author: chloerulesok’s
     for (let i = 0; i < 120; ++i) simulation.tick();
 
-    scatter.selectAll(".dot")
-        .data(data)
-        // .enter().append("g")
-        // .attr('id', d => d.key)
-        
-        // .data(d => d)
-        // .data(d => {
-        //     console.log(d)
-        //     return [d.values];
-        // })
-        // .data(o => {
-        //     console.log(o.values)
-        //     return o.values;
-        // })
-        .enter().append("circle")
-        .attr('id', d => d.stel_terecht)
-        .attr("class", "dot")
-        .attr("r", 3)
-        .attr("cx", function (d, i) {
-          
-            
-
-            // return i == 0 ? 20 : i * 20
-            console.log(d, 'fwef')
-            return d.plek == 0 ? 10 : d.plek * 10
-            
-        })
-
-        .attr("cy", function (d, i, j) {
-            
-            // if(d.parentLength > 10){
-            //     return d.y + (d.migratieachtergrond == 'Deelnemers met migratieachtergrond' ? 15 : 0)
-            // }
-
-
-            return d.y + (d.migratieachtergrond == 'Deelnemers met migratieachtergrond' ? 5 : -5)
-
-           
-        })
-        // .attr("cx", function (d, i) {
-        //     return d.x;
-        // })
-        .style("fill", d=> colour(d.migratieachtergrond));
-
-
-    //    console.log( scatter.selectAll(".dot")
-    //     .data(nestedRightfulness))
-
-    //     scatter.selectAll(".dot")
-    //     .data(nestedRightfulness.length)
-    //     .attr("cx", function (d, i) {
-          
-    //         // return d.x;
-
-    //         return i == 0 ? 10 : i * 10 + 10;
-            
-    //     })
+    update(data)
 
     
-        // svg.select('.tick text')
-        //     // .style('fill', 'yellow')
-        //     // .select('text')
-        //     // .data(population)
-        //     // .enter()
-        //     // .append('text')
 
-        //         .text('fwefwefwef')
+    function update(data) {
+        scatter.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr('id', d => d.stel_terecht)
+            .attr("class", "dot")
+            .attr("r", 3)
+            .attr("cx", function (d, i) {
+
+                // return i == 0 ? 20 : i * 20
+                // console.log(d, 'fwef')
+                return d.plek == 0 ? 10 : d.plek * 10
+
+            })
+
+            .attr("cy", function (d, i, j) {
+
+                // if(d.parentLength > 10){
+                //     return d.y + (d.migratieachtergrond == 'Deelnemers met migratieachtergrond' ? 15 : 0)
+                // }
+
+                return d.y + (d.migratieachtergrond == 'Deelnemers met migratieachtergrond' ? 5 : -5)
+
+            })
+
+            .style("fill", d => colorscale(d.migratieachtergrond));
+    }
+
+
+
+    const consequenceMenu = d3.select('nav[data-filter="gevolg-contact"]');
+
+    consequenceMenu
+      .on("click", function(){
+        console.log(event.target.dataset.value);
+      })
+
+    const nestedConsequence = d3.nest()
+      .key(d => d.polben_gevolg_controle)
+      .entries(data);
+
+      console.log(nestedConsequence)
+
+    function selectData(category) {
+
+        // Wrote this myself.
+        const newArray = doubleNest.filter(item => {
+          return item.key == category
+        }).reduce(item => item)
+    
+        console.log(newArray)
+        update(newArray.values)
+    
+        return;
+    
+      }
+
+    createLegend()
+    function createLegend() {
+        // Wrote this myself
+        const legendWrapper = svg.append('g')
+          .attr("class", "legend");
+    
+    
+        legendWrapper
+          .append("rect")
+          .attr("x", 5)
+          .attr("class", "legend-wrapper");
+        // .on('click', console.log('f'));
+        let clickCount = 0;
+        const legendContainer = legendWrapper.append('g')
+          .attr('class', 'legend-container')
+          .attr('data-legend', 'container') 
+          .on('click', function () {
+    
+            // console.log(event.target)\
+    
+            /* Event delegation (a little ugly, yet better than multiple eventlisteners)
+    
+              Works pretty much the same way as a normal eventlistener (event), exept that the 
+              event object is available already within the eventlistener.
+    
+              If the parent of all legend items it's data-legend value equals container,
+              I know I'm in the right place. Itemparent is the legend item it's parent group which holds
+              the text and the rect. That group contains the category and I can put an active class on it.
+    
+            */
+            const upperParent = event.target.parentNode.parentNode.dataset.legend;
+    
+    
+            if (upperParent === 'container') {
+              const itemParent = event.target.parentNode;
+    
+              legendContainer.selectAll('.legend-item')
+                .classed('active', false)
+                .attr('class', 'legend-item');
+    
+              // if(clickCount % 2 === 0){
+              //   console.log('fefefef')
+              itemParent.classList.add('active');
+              //   clickCount++;
+              // }else{
+              //   clickCount = 0;
+              //   console.log('yeaa?')
+              //   itemParent.classList.remove('active');  
+              // }
+    
+              // Call function and give category name of the legend-item group as argument
+              selectData(itemParent.dataset.category)
+            }
+    
+    
+          })
+    
+        const legend = legendContainer.selectAll(".legend")
+          .data(colorscale.domain())
+          .enter()
+          .append("g")
+          .attr("class", "legend-item")
+          .attr('data-category', d => d)
+          .attr("transform", function (d, i) {
+            return "translate(0," + i * 35 + ")";
+          })
+          .on('click', function () {
+            // selectData(d3.event.target.parentNode.dataset.category)
+            // console.log(d)
+          });
+    
+        legend.append("circle")
+          .attr("cx", 185 - 5)
+          .attr("cy", 9)
+          .attr("r", 10)
+          .style("fill", colorscale);
+    
+        legend.append("text")
+          .attr("x", 200)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .attr("class", "legend-text")
+    
+          .text(function (d) {
+            return d;
+          })
+    
+        legendWrapper
+          .style("transform", `translate(0px, ${height + 5}px)`);
+    
+      }
+
 }
