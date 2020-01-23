267b2f862b36c4be04e7f7f812c967f7bc4d70aa
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-01-23 15:29:20 +0100

    test

--- routes/Get.js
+++ routes/Get.js
@@ -57,7 +57,7 @@ router.get('/users', (req, res)=>{
         
         .then(data => {
             // res.json(data);
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+            res.setHeader('Access-Control-Allow-Origin', 'https://epic-mayer-5c0b28.netlify.com');
             res.header("Content-Type",'application/json');
             res.send(data)
             res.end()
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const path = require('path')
 
 const jsonArray = require('./src/helpers/dataConverting')
 
-
-
 app.use(express.static(__dirname + '/dist'));
  
 app.get('/', function (req, res) {
@@ -18,78 +16,6 @@ app.get('/', function (req, res) {
 })
 
 
-
-// app.use('/api/post/', PostData)
-
 app.use('/api/get/', getUsers)
-
-
-// app.get('/writecsv', (req, res)=>{
-//   function hasMigrationBg(json){
-//     const test = json.map(response => {
-
-
-//         if(response.geboorteland_respondent != 'Nederland'){
-//             Object.assign(response, {afkomst :response.geboorteland_respondent})
-//             return response;
-//         }else if(response.geboorteland_vader != 'Nederland' || response.geboorteland_moeder){
-//             Object.assign(response, {
-//                 afkomst : 
-//                     (response.geboorteland_vader != 'Nederland') ? 
-//                     response.geboorteland_vader : response.geboorteland_moeder
-                
-//             })
-           
-//             return response;
-//         }
-//     })
-// return test;
-// }
-
-
-// const { parse } = require('json2csv');
- 
-// const fields = ['field1', 'field2', 'field3'];
-
- 
-
-
-
-// jsonArray('data/Testdata_waardelabels.csv')
-
-
-
-
-//     // .then(data => console.log(data[0]) )
-//     .then((data) => {
-      
-//       return hasMigrationBg(data)
-//     }).then(data => {
-
-//       const fields = data.map(item=> Object.keys(item)).reduce(item => item)
-//         const opts = { fields };
-//         const csv = parse(data, opts);
-      
-//       try {
-        
-//         console.log(csv);
-
-//         fs.writeFile('./testmap', csv)
-
-
-//       } catch (err) {
-//         console.error(err);
-//       }
-      
-      
-//     })
-
-
-
-// })
-
-
-
-
 
 app.listen(4000)
